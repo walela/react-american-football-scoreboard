3e5095a029d8c476f13eeb95fb73b67fd1584501
Austin Walela <austinmasibo@gmail.com>
2019-11-05 11:29:15 +0300

    added touchdown and field goal click handlers

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  let [homeScore, setHomeScore] = useState(32);
-  let [awayScore, setAwayScore] = useState(32);
+  let [homeScore, setHomeScore] = useState(12);
+  let [awayScore, setAwayScore] = useState(12);
+
+  const touchDownCounter = side => {
+    if (side === "home") {
+      sethomeScore(homeScore + 7);
+    } else {
+      setawayScore(awayScore + 7);
+    }
+  };
+
+  const fieldGoalCounter = side => {
+    if (side === "home") {
+      sethomeScore(homeScore + 3);
+    } else {
+      setawayScore(awayScore + 3);
+    }
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -16,12 +33,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -29,7 +46,7 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
